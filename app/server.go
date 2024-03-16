f60fc945fc5fb6397f29fb11e9e3621244fdacfe
gnv <gajjarapusai1@gmail.com>
2024-03-16 14:44:27 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -13,9 +13,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	var c net.Conn
+	c, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	var data []byte
+	_, err = c.Read(data)
+	if err != nil {
+		fmt.Println("Error reading data: ", err.Error())
+	}
+	_, err = c.Write([]byte("HTTP/1.1 200 OK /r/n/r/n"))
+	if err != nil {
+		fmt.Println("Error writing data: ", err.Error())
+	}
+
+	c.Close()
 }
