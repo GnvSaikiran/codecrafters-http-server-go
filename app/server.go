ede6d57ec3013505229323c5c20f1ea600e13686
gnv <gajjarapusai1@gmail.com>
2024-03-16 15:00:44 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -7,13 +7,14 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", "0.0.0.0:4000")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
 	var c net.Conn
+
 	c, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
@@ -30,5 +31,6 @@ func main() {
 		fmt.Println("Error writing data: ", err.Error())
 	}
 
-	c.Close()
+	defer c.Close()
+
 }
