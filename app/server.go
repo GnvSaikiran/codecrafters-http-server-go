30cc212ec6fbdad3bdaaa0718d7b5696f52449b7
gnv <gajjarapusai1@gmail.com>
2024-03-16 15:12:43 +0530

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", "0.0.0.0:4000")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -21,11 +22,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	var data []byte
+	data := make([]byte, 2048)
 	_, err = c.Read(data)
 	if err != nil {
 		fmt.Println("Error reading data: ", err.Error())
 	}
+	str := string(data)
+	fields := strings.Fields(str)
+	path := fields[1]
+
+	if path != "/" {
+		_, err = c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing data: ", err.Error())
+		}
+	}
+
 	_, err = c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	if err != nil {
 		fmt.Println("Error writing data: ", err.Error())
